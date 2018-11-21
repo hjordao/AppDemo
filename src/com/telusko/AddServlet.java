fa28f5dab02610aac0a6409aa39c512c0cc5e56d
Helder Jordão <hjordao@hotmail.com>
2018-11-21 11:54:23 +0000

    DemoApp: httpservlet cookies

--- src/com/telusko/AddServlet.java
+++ src/com/telusko/AddServlet.java
@@ -1,14 +1,11 @@
 package com.telusko;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 
@@ -17,9 +14,10 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
 		
-		// Session Management
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
+		// Session Management - Cookies
+		Cookie cookie = new Cookie("k", k+"");
+		res.addCookie(cookie);
+		
 		res.sendRedirect("sq");
 		
 		
